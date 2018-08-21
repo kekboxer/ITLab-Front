3fa51da95e261e99327ac3503c5d62407d25bded
Ivan <reide740@gmail.com>
2018-08-21 16:38:03 +0300

    Made JWT invalidation when the time expires

--- src/modules/profile/index.ts
+++ src/modules/profile/index.ts
@@ -9,11 +9,55 @@ import { ProfileState } from './types';
 
 export * from './types';
 
+const urlBase64Decode = (str: string) => {
+  let output = str.replace(/-/g, '+').replace(/_/g, '/');
+  switch (output.length % 4) {
+    case 0:
+      break;
+    case 2:
+      output += '==';
+      break;
+    case 3:
+      output += '=';
+      break;
+    default:
+      // tslint:disable-next-line:no-string-throw
+      throw 'Illegal base64url string!';
+  }
+  return decodeURIComponent(escape(atob(output)));
+};
+
+const decodeToken = (token: string = '') => {
+  if (token === null || token === '') {
+    return { upn: '' };
+  }
+  const parts = token.split('.');
+  if (parts.length !== 3) {
+    throw new Error('JWT must have 3 parts');
+  }
+  const decoded = urlBase64Decode(parts[1]);
+  if (!decoded) {
+    throw new Error('Cannot decode the token');
+  }
+  return JSON.parse(decoded);
+};
+
 const initializeToken = (): string | undefined => {
   const token = localStorage.getItem('user-token');
-  if (token) {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-    return token;
+  if (token == null) {
+    return;
+  }
+
+  try {
+    const expirationDate: number = decodeToken(token).exp * 1000;
+    const currentDate: number = Date.now();
+
+    if (currentDate < expirationDate) {
+      axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+      return token;
+    }
+  } catch (error) {
+    return;
   }
 };
 
