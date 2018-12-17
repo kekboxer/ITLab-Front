3aa2993d2dbc046363d3a62efe0cb07768d48fec
Ivan <reide740@gmail.com>
2018-12-17 13:19:42 +0300

    Added summary

--- src/views/events/EventsPage.vue
+++ src/views/events/EventsPage.vue
@@ -4,10 +4,32 @@
     <page-content :loading="loadingInProcess">
       <template slot="header">События</template>
       <template slot="header-button">
-        <b-button variant="success" to="events/edit/new" v-if="canEdit">Добавить</b-button>
+        <b-button
+          variant="success"
+          to="events/edit/new"
+          v-if="canEdit"
+        >Добавить</b-button>
       </template>
 
-      <b-row v-for="event in eventsCurrent" :key="event.id">
+      <b-row>
+        <b-col
+          cols="12"
+          md="auto"
+          class="ml-md-auto mr-md-3"
+        >
+          <b-button
+            variant="secondary"
+            style="width: 100%"
+            @click="downloadSummary()"
+          >Итоги для повышки <icon name="file-excel"></icon>
+          </b-button>
+        </b-col>
+      </b-row>
+
+      <b-row
+        v-for="event in eventsCurrent"
+        :key="event.id"
+      >
         <b-col>
           <event-item :event="event"></event-item>
         </b-col>
@@ -15,7 +37,10 @@
 
       <b-row>
         <b-col>
-          <div class="load-more" @click="togglePastEvents">
+          <div
+            class="load-more"
+            @click="togglePastEvents"
+          >
             <strong>
               {{ eventsShowPast ? "Скрыть" : "Показать" }} прошедшие события
             </strong>
@@ -24,7 +49,10 @@
       </b-row>
 
       <div v-if="eventsShowPast">
-        <b-row v-for="event in eventsPast" :key="event.id">
+        <b-row
+          v-for="event in eventsPast"
+          :key="event.id"
+        >
           <b-col>
             <event-item :event="event"></event-item>
           </b-col>
@@ -39,19 +67,23 @@
 
 <!-- SCRIPT BEGIN -->
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import { RouteConfig } from 'vue-router';
-import axios from 'axios';
+import { Component, Vue } from "vue-property-decorator";
+import { RouteConfig } from "vue-router";
+import axios from "axios";
 
-import CEventItem from '@/components/EventItem.vue';
-import CPageContent from '@/components/layout/PageContent.vue';
+import CEventItem from "@/components/EventItem.vue";
+import CPageContent from "@/components/layout/PageContent.vue";
+
+import Icon from "vue-awesome/components/Icon";
+
+import "vue-awesome/icons/file-excel";
 
 import {
   IEvent,
   IEventType,
   EVENTS_FETCH_ALL,
   EVENTS_GET_ALL
-} from '@/modules/events';
+} from "@/modules/events";
 
 const createEventSortPredicate = (asc: boolean = true) => (
   a: IEvent,
@@ -68,8 +100,9 @@ const createEventSortPredicate = (asc: boolean = true) => (
 
 @Component({
   components: {
-    'event-item': CEventItem,
-    'page-content': CPageContent
+    Icon,
+    "event-item": CEventItem,
+    "page-content": CPageContent
   }
 })
 export default class EventsPage extends Vue {
@@ -93,7 +126,7 @@ export default class EventsPage extends Vue {
       .dispatch(EVENTS_FETCH_ALL, {
         dateBegin: this.eventsShowPast ? undefined : this.currentDate
       })
-      .then((result) => {
+      .then(result => {
         this.loadingInProcess = false;
       });
   }
@@ -108,6 +141,22 @@ export default class EventsPage extends Vue {
     }
 
     this.eventsShowPast = !this.eventsShowPast;
+  }
+
+  public async downloadSummary() {
+    let data: string = "";
+    try {
+      const result = await axios.get("/summary");
+      data = result.data;
+    } catch (body) {
+      data = body.error;
+    } finally {
+      const a = document.createElement("a");
+      const blob = new Blob(["\ufeff", data], { type: "test/csv" });
+      a.href = window.URL.createObjectURL(blob);
+      a.download = "summary.csv";
+      a.click();
+    }
   }
 
   // Computed data //
@@ -127,13 +176,13 @@ export default class EventsPage extends Vue {
   }
 
   get canEdit(): boolean {
-    return this.$g.hasRole('CanEditEvent');
+    return this.$g.hasRole("CanEditEvent");
   }
 }
 
 export const eventsPageRoute: RouteConfig = {
-  path: '/events',
-  name: 'EventsPage',
+  path: "/events",
+  name: "EventsPage",
   component: EventsPage
 };
 </script>
@@ -142,7 +191,7 @@ export const eventsPageRoute: RouteConfig = {
 
 <!-- STYLE BEGIN -->
 <style lang="scss">
-@import '@/styles/general.scss';
+@import "@/styles/general.scss";
 
 .events-page {
   .load-more {
